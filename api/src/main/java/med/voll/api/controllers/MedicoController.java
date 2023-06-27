2e1e2c7a6971b5c50e17496e76b10f25bfa17369
Vinicius Lopes <vinicius6.lopes6@gmail.com>
2023-06-27 16:37:29 -0300

    Exclusão Física e Lógica de Medicos

--- api/src/main/java/med/voll/api/controllers/MedicoController.java
+++ api/src/main/java/med/voll/api/controllers/MedicoController.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,7 +36,7 @@ public class MedicoController {
 
 	@GetMapping
 	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort= {"nome"}) Pageable paginacao){
-		return repository.findAll(paginacao).map(DadosListagemMedico::new);
+		return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
 		
 	}
 	
@@ -44,5 +46,20 @@ public class MedicoController {
 		var medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
 		
+	}
+//  --> Deleção Física(Excluí do banco de dados) <--	
+//	@DeleteMapping("/{id}")
+//	@Transactional
+//	public void deletar(@PathVariable Long id) {
+//		repository.deleteById(id);
+//	}
+
+	
+//	--> Deleção Lógica(Apenas desativar) <--	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void deletar(@PathVariable Long id) {
+		var medico = repository.getReferenceById(id);
+		medico.excluir();
 	}
 }
